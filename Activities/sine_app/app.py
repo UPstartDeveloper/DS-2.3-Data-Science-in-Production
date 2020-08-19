db72a758b7dce00ddff74ebb89dd201b7c9fddb9
= <zainr7989@gmail.com>
2020-08-19 19:12:35 -0400

    Add Flask app for addition.

---
+++ Activities/sine_app/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, render_template, redirect, url_for
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET', 'POST'])
+def calculate_sum():
+    num1 = request.form.get('num1')
+    num2 = request.form.get('num2')
+    if num1 and num2:
+        sum = float(num1) + float(num2)
+    else:
+        sum = 'Not defined'
+    return render_template(
+        'base.html', num1=num1, num2=num2, sum=sum
+    )
+    # user has inputted numbers
+    if request.method == 'POST':
+        return redirect(url_for('calculate_sum'))
