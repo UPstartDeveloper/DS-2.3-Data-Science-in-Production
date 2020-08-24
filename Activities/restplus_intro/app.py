5bca60359ab07c5c16698f78f0d90be2a03a80f4
= <zainr7989@gmail.com>
2020-08-24 14:42:00 -0400

    Add endpoint for GET requests to intro app.

--- Activities/restplus_intro/app.py
+++ Activities/restplus_intro/app.py
@@ -1,3 +1,9 @@
+import werkzeug
+# This next lime Prevents ImportErrors - flask-restplus currently gets broken by 
+# Werkzeug 1.0.0. Alternatively, you could also downgrade werkzeug 
+# (the package Python uses to interface with an HTTP web server)
+# Note: line 6 must happen BEFORE you import Flask to be able to work!
+werkzeug.cached_property = werkzeug.utils.cached_property
 from flask import Flask
 from flask_restplus import Api, Resource
 
@@ -12,8 +18,14 @@ class Language(Resource):
     """
     This is a class for the API route, we use these rather than functions 
     (which is more like vanilla Flask).
+
+    For now, the base route will be created automatically using
+    a really sleek Swagger documentation - will help us document 
+    information about our API, and try out the endpoints in the browser.
     """
-    pass
+    def get(self):
+        '''Method for the Language class to handle GET requests.'''
+        return {'hey': 'there'}  # jsonify not required like in vanilla Flask
 
 
 # run the app in debug mode locally
